398fb236d9602bae8b28260a34d048e85b19f556
Michael Nusair <mnpcmw6444@gmail.com>
2025-12-03 22:07:18 +0200

    fix

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -1,1 +1,10 @@
-export type HW = "Hello World";
+export type { RectangleItem } from "./types/rectangle";
+export type {
+  Design,
+  DesignListItem,
+  DesignIssue,
+  DesignStatus,
+} from "./types/design";
+export type { ApiError, UploadDesignResponse } from "./types/api";
+export type { ParsedSVGResult } from "./types/parser";
+export { isDesignStatus, isDesignIssue } from "./types/guards";
