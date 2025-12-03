722207e193938720b3d934c9b5914a2e69237146
Michael Nusair <mnpcmw6444@gmail.com>
2025-12-03 22:09:36 +0200

    added exaples and app placeholder

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@svg-processor/shared-types": path.resolve(
+        __dirname,
+        "../../libs/shared-types/src"
+      ),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
